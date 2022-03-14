ca4a4205cddd293a72c32dfed441a4ea80024a60
oladimejiadesina-123 <adesinaoladimeji04@gmail.com>
2022-03-14 23:07:13 +0100

    added new files and folder

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HEROES } from '../mock-heroes'
 import { HeroService } from '../hero.service';
+import { MesssagesService } from '../messsages.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,14 +14,16 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messsagesService: MesssagesService) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
 
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messsagesService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
   getHeroes(): void {
      this.heroService.getHeroes()
