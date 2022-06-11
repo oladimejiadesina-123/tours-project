5d52b1a0ecb200f979a3a1bb762f1f8e52b44552
oladimejiadesina-123 <adesinaoladimeji04@gmail.com>
2022-06-11 22:32:33 +0100

    added new styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
 import { RouterModule, Routes } from '@angular/router';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'detail/:id', component: HeroDetailComponent},
   { path: 'heroes', component: HeroesComponent}
 ];
 
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero'
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router'
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common'
 
 
 @Component({
@@ -8,12 +11,20 @@ import { Hero } from '../hero'
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+hero: Hero | undefined;
 
-@Input() hero?: Hero;
-
-  constructor() { }
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location ) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+ getHero(): void {
+  const id = Number(this.route.snapshot.paramMap.get('id'));
+  this.heroService.getHero(id)
+  .subscribe( hero => this.hero = hero);
+}
+  goBack(): void {
+    this.location.back();
+  }
 }
